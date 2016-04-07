6d43d3f07434e6de1d31e7dff7c0a428ef7bd2cd
Istvan Karady <ikarady@temenos.com>
2016-04-07 12:03:49 +0100

    completing unit test for link creation from dynamic resource

--- interaction-core/src/test/java/com/temenos/interaction/core/hypermedia/link/TestLinkGenerator.java
+++ interaction-core/src/test/java/com/temenos/interaction/core/hypermedia/link/TestLinkGenerator.java
@@ -1,14 +1,12 @@
 package com.temenos.interaction.core.hypermedia.link;
 
 import com.temenos.interaction.core.MultivaluedMapImpl;
-import com.temenos.interaction.core.command.InteractionContext;
 import com.temenos.interaction.core.hypermedia.*;
 import com.temenos.interaction.core.web.RequestContext;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import javax.ws.rs.core.MultivaluedMap;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,45 +15,55 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import static org.mockito.Matchers.anyObject;
-
 
 /**
  * Created by ikarady on 06/04/2016.
  */
 public class TestLinkGenerator {
 
-    //@Test
+    @Test
     public void testCreateLinkFromDynamicResource() {
-        //ResourceStateMachine engine = new ResourceStateMachine(mock(ResourceState.class), new BeanTransformer());
-        RequestContext.setRequestContext(new RequestContext("test", null, null));
+        RequestContext.setRequestContext(new RequestContext("/test", null, null));
         ResourceStateMachine engineMock = Mockito.mock(ResourceStateMachine.class);
         ResourceStateAndParameters resourceStateAndParameters = new ResourceStateAndParameters();
         resourceStateAndParameters.setState(mockTarget("/testDynamic"));
-        resourceStateAndParameters.setParams(new ParameterAndValue[] {new ParameterAndValue(null, null)});
-        Mockito.when(engineMock.resolveDynamicState(
-                Mockito.mock(DynamicResourceState.class),
-                Mockito.mock(Map.class),
-                Mockito.mock(InteractionContext.class)))
-                .thenReturn(resourceStateAndParameters);
+        resourceStateAndParameters.setParams(new ParameterAndValue[] {new ParameterAndValue("filter2", "564")});
+        DynamicResourceState dynamicResourceStateMock = mockDynamicTarget(null);
         Map<String,String> uriParameters = new HashMap<String,String>();
         uriParameters.put("filter", "{nodeId}");
         Transition t = new Transition.Builder()
                 .source(mock(ResourceState.class))
-                .target(mockTarget("/test"))
+                .target(dynamicResourceStateMock)
                 .uriParameters(uriParameters)
                 .build();
         MultivaluedMap<String,String> queryParameters = new MultivaluedMapImpl<String>();
         queryParameters.add("nodeId", "123");
+        Map<String, Object> transitionProperties = new HashMap<String, Object>();
+        transitionProperties.put("filter", "{nodeId}");
+        transitionProperties.put("nodeId", "123");
+        Mockito.when(engineMock.resolveDynamicState(
+                dynamicResourceStateMock,
+                transitionProperties,
+                null))
+                .thenReturn(resourceStateAndParameters);
+        Mockito.when(engineMock.getTransitionProperties(t, null, new MultivaluedMapImpl<String>(), queryParameters)).thenReturn(transitionProperties);
+
         LinkGenerator linkGenerator = new LinkGenerator(engineMock, t, null);
         linkGenerator.setAllQueryParameters(true);
         Collection<Link> links = linkGenerator.createLink(new MultivaluedMapImpl<String>(), queryParameters, null);
         Link result = (!links.isEmpty()) ? links.iterator().next() : null;
-        assertEquals("/baseuri/test?$filter=123", result.getHref());
+        assertEquals("/test/testDynamic?filter=123&filter2=564", result.getHref());
     }
 
     private ResourceState mockTarget(String path) {
         ResourceState target = mock(ResourceState.class);
+        when(target.getPath()).thenReturn(path);
+        when(target.getRel()).thenReturn("collection");
+        return target;
+    }
+
+    private DynamicResourceState mockDynamicTarget(String path) {
+        DynamicResourceState target = mock(DynamicResourceState.class);
         when(target.getPath()).thenReturn(path);
         return target;
     }
